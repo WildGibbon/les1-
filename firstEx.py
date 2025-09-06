3aeb39f7dafc2d1fc4115f5c08f34d2f205fbd5d
Ainur Nugumanov <chelovekbola@mail.ru>
2025-09-06 10:01:45 +0300

    Create firstEx.py

---
+++ firstEx.py
@@ -0,0 +1,10 @@
+from math import *
+
+a = int(input())
+b = int(input())
+
+z1 = (cos(radians(a)) - cos(radians(b)))**2 - (sin(radians(a)) - sin(radians(b)))**2
+z2 = -4*sin((radians(a-b))/2)**2*cos(radians(a+b))
+
+print(round(z1, 3))
+print(round(z2, 3))
